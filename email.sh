0841040f82eaf16f425fa5e36086a3aa0cc0bfdd
Likith1705 <114089035+Likith1705@users.noreply.github.com>
2022-12-20 22:12:11 +0530

    Delete email.sh

--- email.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-perc=`df -h . | awk -F " " 'NR==2{print $5}' | sed 's/%/ /g'`
-if [ $perc -ge 18 ]; then
-        echo "/dev/root drive size reached maximum thershold value and current usage is $perc percent" | mail -s 'disk usage' likithbs703@gmail.com
-fi
